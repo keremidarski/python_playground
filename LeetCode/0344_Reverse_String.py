0667ea379c2c4d9de8f7944cfab17b54ff950ed3
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-17 18:55:49 +0300

    Update 0344_Reverse_String.py

--- LeetCode/0344_Reverse_String.py
+++ LeetCode/0344_Reverse_String.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/reverse-string/
+
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
 def reverse_string_inplace(s):
     return s.reverse()
 
