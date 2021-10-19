bc5194ec1b70407b5b37b57159aca737eb331bcc
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-19 10:45:59 +0300

    Update 0007_Reverse_Integer.py

--- LeetCode/0007_Reverse_Integer.py
+++ LeetCode/0007_Reverse_Integer.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/reverse-integer/
+
+# Given a signed 32-bit integer x, return x with its digits reversed.
+# If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
 def reverse_integer(x):
     result = 0
 
@@ -13,4 +19,5 @@ def reverse_integer(x):
 
     return 0 if result > pow(2, 31) else result * symbol
 
+# Expected: 21
 print(reverse_integer(120))
