61cab02c143f13772b693b6b8a983be823931b2c
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-21 17:48:29 +0300

    Update 0771_Jewels_and_Stones.py

--- LeetCode/0771_Jewels_and_Stones.py
+++ LeetCode/0771_Jewels_and_Stones.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/jewels-and-stones/
+
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. 
+# Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
 def jewels_in_stones(jewels, stones):
     counter = 0
     for stone in stones:
@@ -6,4 +12,5 @@ def jewels_in_stones(jewels, stones):
                 counter += 1
     return counter
 
+# Expected: 3
 print(jewels_in_stones("aA", "aAAbbbb"))
