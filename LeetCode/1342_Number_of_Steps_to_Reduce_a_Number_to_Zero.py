297257611b2f6b8ad7eafea24378f00e6c1fbfdb
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-23 13:03:52 +0300

    Update 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py

--- LeetCode/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
+++ LeetCode/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -1,3 +1,8 @@
+# https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+# Given an integer num, return the number of steps to reduce it to zero.
+# In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
 def number_of_steps(num):
     counter = 0
         
@@ -10,4 +15,5 @@ def number_of_steps(num):
             counter += 1
     return counter
 
+# Expected: 6
 print(number_of_steps(14))
