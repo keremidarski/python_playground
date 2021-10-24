bab9d9c4e00d37c4e7e9a84751f7d5fb605d40af
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-24 17:21:06 +0300

    Update 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py

--- LeetCode/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ LeetCode/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+# Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+# That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+# Return the answer in an array.
+
 def smaller_numbers_than_current(nums):
     result = []
         
@@ -10,4 +16,4 @@ def smaller_numbers_than_current(nums):
     return result
 
 # Expected: [4, 0, 1, 1, 3]
-print(smaller_numbers_than_current([8,1,2,2,3]))
+print(smaller_numbers_than_current([8, 1, 2, 2, 3]))
