4a2190c381ff88c9f35ca67bb674834881f30d60
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-25 16:37:21 +0300

    Update 1431_Kids_With_the_Greatest_Number_of_Candies.py

--- LeetCode/1431_Kids_With_the_Greatest_Number_of_Candies.py
+++ LeetCode/1431_Kids_With_the_Greatest_Number_of_Candies.py
@@ -1,3 +1,11 @@
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+# There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has,
+# and an integer extraCandies, denoting the number of extra candies that you have.
+# Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies,
+# they will have the greatest number of candies among all the kids, or false otherwise.
+# Note that multiple kids can have the greatest number of candies.
+
 def kids_with_candies(candies, extraCandies):
     result = []
     max_candy = candies.copy()
@@ -12,4 +20,5 @@ def kids_with_candies(candies, extraCandies):
 
     return result
 
-print(kids_with_candies([4,2,1,1,2], 1))
+# Expected: [true, false, false, false, false]
+print(kids_with_candies([4, 2, 1, 1, 2], 1))
