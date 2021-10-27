411a6226d6af4afb90a5d414afcfd8a3e2506071
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-10-27 09:43:12 +0300

    Update 1470_Shuffle_the_Array.py

--- LeetCode/1470_Shuffle_the_Array.py
+++ LeetCode/1470_Shuffle_the_Array.py
@@ -1,3 +1,8 @@
+# https://leetcode.com/problems/shuffle-the-array/
+
+# Given the array nums consisting of 2n elements in the form [x1, x2, ..., xn, y1, y2, ..., yn].
+# Return the array in the form [x1, y1, x2, y2, ..., xn, yn].
+
 def shuffle_array(nums):
     result = []
     x_array = []
@@ -11,3 +16,6 @@ def shuffle_array(nums):
         result.append(x_array[i])
         result.append(y_array[i])
     return result
+
+# Expected: [2, 3, 5, 4, 1, 7]
+print(shuffle_array([2, 5, 1, 3, 4, 7], 3))
