e709002e3b90b6c340f1ddd89a737d55a6879000
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-11-10 10:53:21 +0200

    Update 0344_Reverse_String.py

--- LeetCode/0344_Reverse_String.py
+++ LeetCode/0344_Reverse_String.py
@@ -5,6 +5,6 @@
 def reverse_string_inplace(s):
     return s.reverse()
 
-# Expected: ['o','l','l','e','h']
-s = ['h','e','l','l','o']
+# Expected: ['o', 'l', 'l', 'e', 'h']
+s = ['h', 'e', 'l', 'l', 'o']
 reverse_string_inplace(s)
