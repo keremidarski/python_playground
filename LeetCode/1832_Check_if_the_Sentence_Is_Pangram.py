f13fda2b6b625f43b67e28dd6413db99c7ac317c
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-11-17 13:06:36 +0200

    Update 1832_Check_if_the_Sentence_Is_Pangram.py

--- LeetCode/1832_Check_if_the_Sentence_Is_Pangram.py
+++ LeetCode/1832_Check_if_the_Sentence_Is_Pangram.py
@@ -1,4 +1,4 @@
 def is_pangram(sentence):
     return 26 == len(set(list(sentence)))
 
-print(is_pangram("thequickbrownfoxjumpsoverthelazydog"))
+print(is_pangram('thequickbrownfoxjumpsoverthelazydog'))
