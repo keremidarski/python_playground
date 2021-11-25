26269372c8c21633182cf164c0080e86f29b13ad
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-11-25 09:22:06 +0200

    Update c01p02_sum_of_digits.py

--- Python 101 Forever/C01 - Python Basics/c01p02_sum_of_digits.py
+++ Python 101 Forever/C01 - Python Basics/c01p02_sum_of_digits.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/13-C01P02
+
 def sum_of_digits(nums):
     result = 0
 
@@ -8,4 +11,5 @@ def sum_of_digits(nums):
 
     return result
 
+# Expected: 1
 print(sum_of_digits(-10))
