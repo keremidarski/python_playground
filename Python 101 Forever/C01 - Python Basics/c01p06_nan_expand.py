087a2197a7dbe46d02f2852cef92d02cde672769
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-11-28 15:59:27 +0200

    Update c01p06_nan_expand.py

--- Python 101 Forever/C01 - Python Basics/c01p06_nan_expand.py
+++ Python 101 Forever/C01 - Python Basics/c01p06_nan_expand.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/17-C01P06
+
 def nan_expand(times):
     nan = 'NaN'
     not_a = 'Not a '
@@ -8,4 +11,5 @@ def nan_expand(times):
     
     return result
 
+# Expected: 'Not a Not a Not a NaN'
 print(nan_expand(3))
