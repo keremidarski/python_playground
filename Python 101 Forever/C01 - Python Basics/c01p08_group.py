ca190b9224a0d4e960d32f78d911ad7a808f2b3c
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-11-30 13:15:24 +0200

    Update c01p08_group.py

--- Python 101 Forever/C01 - Python Basics/c01p08_group.py
+++ Python 101 Forever/C01 - Python Basics/c01p08_group.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/19-C01P08
+
 def group(items):
     result = []
     temp_group = []
@@ -23,5 +26,6 @@ tests = [group([1, 1, 1, 2, 3, 1, 1]) == [[1, 1, 1], [2], [3], [1, 1]],
         group([1]) == [[1]],
         group([1, 1, 1, 1]) == [[1, 1, 1, 1]]]
 
+# Expected: True
 for test in tests:
     print(test)
