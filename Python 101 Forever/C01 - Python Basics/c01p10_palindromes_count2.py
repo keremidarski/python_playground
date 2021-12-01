76c85d6dc543080a5694688ce5f5b28c58e21647
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-01 12:05:45 +0200

    Update c01p10_palindromes_count2.py

--- Python 101 Forever/C01 - Python Basics/c01p10_palindromes_count2.py
+++ Python 101 Forever/C01 - Python Basics/c01p10_palindromes_count2.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/21-C01P10
+
 def string_reverse(s):
     return ''.join(reversed(s))
 
@@ -22,3 +25,28 @@ def palindromes_count(n):
         if CACHE[x]:
             count += 1
     return count
+
+tests = [
+    (10, 0),
+    (20, 1),
+    (30, 2),
+    (101, 10),
+    (200, 19),
+    (43539, 525),
+    (4247, 132),
+    (48877, 577),
+    (94012, 1029),
+    (62560, 715),
+    (92009, 1009),
+    (63176, 721),
+    (67409, 763),
+    (62834, 718),
+    (77420, 863),
+    (99999, 1089),
+]
+
+for n, expected in tests:
+    result = palindromes_count(n)
+    
+    # Expected: True
+    print(result == expected)
