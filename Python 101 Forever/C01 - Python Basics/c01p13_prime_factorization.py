e00bc28e38f5405bd158d226b89bf3eb53736d51
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-03 15:27:09 +0200

    Update c01p13_prime_factorization.py

--- Python 101 Forever/C01 - Python Basics/c01p13_prime_factorization.py
+++ Python 101 Forever/C01 - Python Basics/c01p13_prime_factorization.py
@@ -1,17 +1,24 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/24-C01P13
+
 def is_prime(n):
     counter = 0
 
     for i in range(1, n + 1):
         if n % i == 0:
             counter += 1
+            
     return counter == 2
+
 
 def next_prime(n):
     n += 1
 
     while not is_prime(n):
         n += 1
+        
     return n
+
 
 def prime_factorization(n):
     result = []
@@ -32,4 +39,5 @@ def prime_factorization(n):
 
     return result
 
+# Expected: [(2, 1), (5, 1)]
 print(prime_factorization(10))
