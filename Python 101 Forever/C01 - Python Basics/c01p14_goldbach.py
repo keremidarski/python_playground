615cd96bcb8b2727d23d5c59681b18ca53701bfa
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-03 15:28:42 +0200

    Update c01p14_goldbach.py

--- Python 101 Forever/C01 - Python Basics/c01p14_goldbach.py
+++ Python 101 Forever/C01 - Python Basics/c01p14_goldbach.py
@@ -1,17 +1,24 @@
+# Problem description:
+# https://github.com/HackBulgaria/Python-101-Forever/tree/master/C01-Python-Basics/25-C01P14
+
 def is_prime(n):
     counter = 0
 
     for i in range(1, n + 1):
         if n % i == 0:
             counter += 1
+            
     return counter == 2
+
 
 def next_prime(n):
     n += 1
 
     while not is_prime(n):
         n += 1
+        
     return n
+
 
 def goldbach(n):
     result = []
@@ -27,6 +34,8 @@ def goldbach(n):
             p = next_prime(p)
         else:
             p = next_prime(p)
+            
     return result
 
-print(goldbach(5))
+# Expected: [(3, 7), (5, 5)]
+print(goldbach(10))
