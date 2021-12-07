e32deab4be177f168f1600e3371b20382d3fbc70
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-07 17:37:53 +0200

    Add files via upload

---
+++ Python 101 Forever/C02 - Linux and Python Setup/c02p01_reduce_file_path2.py
@@ -0,0 +1,29 @@
+def reduce_file_path(path):
+    result = []
+
+    for i in path.split('/'):
+        if i == '' or i == '.':
+            continue
+
+        if i == '..':
+            if len(result) > 0:
+                result.pop()
+                continue
+            else:
+                continue
+
+        result.append(i)
+        
+    return '/' + '/'.join(result)
+
+
+print(reduce_file_path("/") == "/")
+print(reduce_file_path("/srv/../") == "/")
+print(reduce_file_path("/srv/www/htdocs/wtf/") == "/srv/www/htdocs/wtf")
+print(reduce_file_path("/srv/www/htdocs/wtf") == "/srv/www/htdocs/wtf")
+print(reduce_file_path("/srv/./././././") == "/srv")
+print(reduce_file_path("/etc//wtf/") == "/etc/wtf")
+print(reduce_file_path("/etc/../etc/../etc/../") == "/")
+print(reduce_file_path("//////////////") == "/")
+print(reduce_file_path("/..//////.") == "/")
+print(reduce_file_path('/home/.../'))
