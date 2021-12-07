2904cd68f25b8da9cd606126fce71847e8a2f317
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-07 09:57:22 +0200

    Update 1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py

--- LeetCode/1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
+++ LeetCode/1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
@@ -1,6 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/ 
-
-# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
 def subtract_product_and_sum(n):
     digits = []
