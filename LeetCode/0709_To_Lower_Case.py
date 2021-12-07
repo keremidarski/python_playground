4b044fe87ab0c6b05006953bdb04e4882de75431
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-07 09:56:13 +0200

    Update 0709_To_Lower_Case.py

--- LeetCode/0709_To_Lower_Case.py
+++ LeetCode/0709_To_Lower_Case.py
@@ -1,6 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/to-lower-case/
-
-# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
 
 def to_lower_case(str):
     return str.lower()
