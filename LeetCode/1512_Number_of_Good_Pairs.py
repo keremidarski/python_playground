1e17639ee8234420909aaaa46bb6a7ec79dccf8d
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-09 12:40:45 +0200

    Update 1512_Number_of_Good_Pairs.py

--- LeetCode/1512_Number_of_Good_Pairs.py
+++ LeetCode/1512_Number_of_Good_Pairs.py
@@ -1,7 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/number-of-good-pairs/
-
-# Given an array of integers nums, return the number of good pairs.
-# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
 def good_pairs(nums):
     occurances = {}
