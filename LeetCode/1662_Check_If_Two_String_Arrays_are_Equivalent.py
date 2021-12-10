f34cdc2f707725d5d38c82ebe6b90e7e74ecdb5a
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-10 12:40:23 +0200

    Update 1662_Check_If_Two_String_Arrays_are_Equivalent.py

--- LeetCode/1662_Check_If_Two_String_Arrays_are_Equivalent.py
+++ LeetCode/1662_Check_If_Two_String_Arrays_are_Equivalent.py
@@ -1,7 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
-
-# Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-# A string is represented by an array if the array elements concatenated in order forms the string.
 
 def array_strings_are_equal(word1, word2):
     return ''.join(word1) == ''.join(word2)
