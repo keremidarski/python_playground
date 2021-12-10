c7cac47c3a3dd926194b8195d7e260f5037f62e5
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-10 12:40:44 +0200

    Update 1672_Richest_Customer_Wealth.py

--- LeetCode/1672_Richest_Customer_Wealth.py
+++ LeetCode/1672_Richest_Customer_Wealth.py
@@ -1,8 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/richest-customer-wealth/
-
-# You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i-th customer has in the j-th bank.
-# Return the wealth that the richest customer has. A customer's wealth is the amount of money they have in all their bank accounts. 
-# The richest customer is the customer that has the maximum wealth.
 
 def maximum_wealth(accounts):
     max_wealth = 0
