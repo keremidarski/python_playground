33e35d1a2d5dd86726f0ede7f9b63f7364abd6e5
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-12 12:09:19 +0200

    Update 2000_Reverse_Prefix_of_Word.py

--- LeetCode/2000_Reverse_Prefix_of_Word.py
+++ LeetCode/2000_Reverse_Prefix_of_Word.py
@@ -1,11 +1,5 @@
+# Problem description:
 # https://leetcode.com/problems/reverse-prefix-of-word/
-
-# Given a 0-indexed string word and a character ch, reverse the segment of word
-# that starts at index 0 and ends at the index of the first occurrence of ch (inclusive).
-# If the character ch does not exist in word, do nothing.
-# For example, if word = "abcdefd" and ch = "d", then you should reverse the segment
-# that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
-# Return the resulting string.
 
 def reverse_prefix(word, ch):
     prefix = []
@@ -23,4 +17,5 @@ def reverse_prefix(word, ch):
 
     return ''.join(prefix)
 
+# Expected: 'dcbaefd'
 print(reverse_prefix('abcdefd', 'd'))
