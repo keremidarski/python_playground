81962e48184bad47003cc0af1d9cee3d777701cd
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-14 16:28:31 +0200

    Update 01_names.py

--- Programming 0/week 1/01_names.py
+++ Programming 0/week 1/01_names.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/1-IO-Simple-Problems
+
 first_name = input('First name? ')
 second_name = input('Second name? ')
 third_name = input('Third name? ')
