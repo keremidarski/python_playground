91ac4b8091429ca0deb34c85ebbbac99177441f4
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-14 16:30:25 +0200

    Update 05_dice_game.py

--- Programming 0/week 1/05_dice_game.py
+++ Programming 0/week 1/05_dice_game.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/2-If-Elif-Else-Simple-Problems
+
 from random import randint
 
 sides = int(input('Enter dice side: '))
