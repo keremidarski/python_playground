e35cb606a25dba26511b16434dfa04040f5d1f38
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-15 10:41:02 +0200

    Update 08_even_odd.py

--- Programming 0/week 1/08_even_odd.py
+++ Programming 0/week 1/08_even_odd.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/2-If-Elif-Else-Simple-Problems
+
 number = int(input('Enter number: '))
 
 if number % 2 == 0:
