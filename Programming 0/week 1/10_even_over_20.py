8f885a6fbe3fc0abb5b48fc0bc9132e719d1d904
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-15 10:49:49 +0200

    Update 10_even_over_20.py

--- Programming 0/week 1/10_even_over_20.py
+++ Programming 0/week 1/10_even_over_20.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/3-And-Or-Not-In-Problems
+
 number = int(input('Enter number: '))
 
 if number % 2 == 0 and number > 20:
