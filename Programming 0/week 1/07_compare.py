55d1b21a17706c8f53a7c0ea0f9500a82694f478
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-15 10:40:33 +0200

    Update 07_compare.py

--- Programming 0/week 1/07_compare.py
+++ Programming 0/week 1/07_compare.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/2-If-Elif-Else-Simple-Problems
+
 num_a = int(input('Enter a: '))
 num_b = int(input('Enter b: '))
 
