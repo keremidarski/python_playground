bb34a0213f27d854ffa1c60099585ea332bb198a
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-16 11:05:21 +0200

    Update 15_count_to_user.py

--- Programming 0/week 1/15_count_to_user.py
+++ Programming 0/week 1/15_count_to_user.py
@@ -1,7 +1,9 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week1/4-While-Loop-Problems
+
 number = int(input('Enter number: '))
 counter = 1
 
 while counter <= number:
     print(counter)
-
     counter += 1
