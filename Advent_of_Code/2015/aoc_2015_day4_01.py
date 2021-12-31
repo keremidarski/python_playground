fb591b9397c353923f35f8a7a6fa11b09054e5a2
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2021-12-31 12:27:17 +0200

    Add files via upload

---
+++ Advent_of_Code/2015/aoc_2015_day4_01.py
@@ -0,0 +1,18 @@
+# Problem description:
+# https://adventofcode.com/2015/day/4
+
+import hashlib
+
+def find_hash(prefix):
+    n = 0
+
+    while True:
+        key = prefix + str(n)
+        hash = hashlib.md5(f'{key}'.encode("utf-8")).hexdigest()
+
+        if hash.startswith('00000'):
+            return n
+
+        n += 1
+
+print(find_hash('bgvyzdsv'))
