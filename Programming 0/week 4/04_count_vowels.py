ccde77e9743cd016bbea4f20ee653f30ef843e89
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-08 16:29:02 +0200

    Update 04_count_vowels.py

--- Programming 0/week 4/04_count_vowels.py
+++ Programming 0/week 4/04_count_vowels.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week4/4-Count-Vowels-Consonants
+
 def count_vowels_consonants(word):
     result = {'vowels': 0, 'consonants': 0}
 
