299fb70d283f7ead932c3edd1968d2b4c97543be
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-10 17:23:14 +0200

    Update 06_neighbours.py

--- Programming 0/week 4/06_neighbours.py
+++ Programming 0/week 4/06_neighbours.py
@@ -1,13 +1,3 @@
-# def count_zero_neighbours(numbers):
-#     counter = 0
-
-#     for i in numbers:
-#         if numbers[i] + numbers[(i + 1)] == 0:
-#             counter += 1
-#     return counter
-
-# print(count_zero_neighbours([1, -1, 2, -2, 0, 0, 5, -5]))
-
 def count_zero_neighbours(numbers):
     counter = 0
     length = len(numbers)
