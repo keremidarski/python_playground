e5d4cb9ab4deaaae3a7ad7bff3afb2cae11f0ff7
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-13 09:32:45 +0200

    Update 13_matrix.py

--- Programming 0/week 4/13_matrix.py
+++ Programming 0/week 4/13_matrix.py
@@ -1,8 +1,12 @@
+# Problem description:
+# https://youtu.be/VvjWU9fponc
+
 def matrix_dimensions(matr):
     row_count = len(matr)
     column_count = len(matr[0])
 
     return f'The matrix has dimensions: {row_count}x{column_count}'
+
 
 def matrix_sum(matr):
     total_sum = 0
@@ -14,7 +18,9 @@ def matrix_sum(matr):
         for column_index in range(0, m):
             element = matr[row_index][column_index]
             total_sum += element
+            
     return total_sum
+
 
 a = [ [1, 2, 3],
       [4, 5, 6],
