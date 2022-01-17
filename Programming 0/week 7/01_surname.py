6c51335d26c717ebfc8a8178b091b17410d1462e
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-17 11:03:00 +0200

    Update 01_surname.py

--- Programming 0/week 7/01_surname.py
+++ Programming 0/week 7/01_surname.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week7/1-Taken-Name
+
 def taken_name(surname_husband, surname_wife):
     if surname_husband in surname_wife:
         return True
