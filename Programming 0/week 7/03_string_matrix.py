dc519f2a543cafe46d88290254a89a03dc576b97
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-18 10:32:46 +0200

    Update 03_string_matrix.py

--- Programming 0/week 7/03_string_matrix.py
+++ Programming 0/week 7/03_string_matrix.py
@@ -1,16 +1,22 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/week7/3-String-Matrix
+
 def string_matrix(matrix_width, strings):
     for string in strings:
         if len(string) == matrix_width:
             print('|', end='')
+            
             for char in string:
                 print(f' {char}', end=' |')
             print('\n')
         elif len(string) < matrix_width:
             counter = matrix_width
             print('|', end='')
+            
             for char in string:
                 print(f' {char}', end=' |')
                 counter -= 1
+                
             while counter > 0:
                 print(f' X', end=' |')
                 counter -= 1
@@ -18,11 +24,13 @@ def string_matrix(matrix_width, strings):
         elif len(string) > matrix_width:
             counter = matrix_width
             print('|', end='')
+            
             for char in string:
                 print(f' {char}', end=' |')
                 counter -= 1
+                
                 if counter == 0:
                     break
             print('\n')
 
-print(string_matrix(6,["python","gogo","perl","java","haskell","ruby0nRails"]))
+print(string_matrix(6, ['python', 'gogo', 'perl', 'java', 'haskell', 'ruby0nRails']))
