56ee1d567e10cdcd287eb5398971e9efc14a6a8f
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-19 13:51:11 +0200

    Create 2114_Maximum_Number_of_Words_Found_in_Sentences.py

---
+++ LeetCode/2114_Maximum_Number_of_Words_Found_in_Sentences.py
@@ -0,0 +1,12 @@
+# Problem description:
+# https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+def most_words_found(list):
+    result = 0
+        
+    for sentence in sentences:
+        if len(sentence.split(' ')) > result:
+            result = len(sentence.split(' '))
+                
+    return result
+  
