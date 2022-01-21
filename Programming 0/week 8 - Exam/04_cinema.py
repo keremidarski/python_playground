0b74748b8a3feba5c2195edc4a1840e327316c58
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-21 13:25:56 +0200

    Update 04_cinema.py

--- Programming 0/week 8 - Exam/04_cinema.py
+++ Programming 0/week 8 - Exam/04_cinema.py
@@ -1,3 +1,6 @@
+# Problem description:
+# https://github.com/HackBulgaria/Programming0-1/tree/master/exam/4-Cinema-Seats
+
 from sys import maxsize
 
 def least_populated_row(cinema):
@@ -6,14 +9,17 @@ def least_populated_row(cinema):
 
     for index, row in enumerate(cinema):
         counter = 0
+        
         for seat in row:
             if seat == 0:
                 counter += 1
+                
         if counter < max_row and counter != 0:
             max_row = counter
             rows_with_least_zeros = index
     
     return rows_with_least_zeros
+
 
 def order_of_seats(cinema):
     result = []
@@ -25,7 +31,9 @@ def order_of_seats(cinema):
             if seat == 0:
                 result.append((least_populated + 1, index + 1))
                 cinema[least_populated][index] = 1
+    
     return result
+
 
 print(order_of_seats([ [1, 1, 1],
            [1, 0, 0],
