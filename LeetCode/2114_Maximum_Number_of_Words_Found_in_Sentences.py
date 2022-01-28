3018ae5d49819e2bc7cd41866e3623bbf63b7bf6
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-28 16:01:07 +0200

    Update 2114_Maximum_Number_of_Words_Found_in_Sentences.py

--- LeetCode/2114_Maximum_Number_of_Words_Found_in_Sentences.py
+++ LeetCode/2114_Maximum_Number_of_Words_Found_in_Sentences.py
@@ -4,7 +4,7 @@
 def most_words_found(list):
     result = 0
         
-    for sentence in sentences:
+    for sentence in list:
         if len(sentence.split(' ')) > result:
             result = len(sentence.split(' '))
                 
