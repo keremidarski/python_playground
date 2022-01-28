b78be441b20ac60f84458d2720fec8b15c118ae0
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-01-28 16:04:14 +0200

    Add files via upload

---
+++ LeetCode/2108_Find_First_Palindromic_String_in_the_Array.py
@@ -0,0 +1,9 @@
+# Problem description:
+# https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+def first_palindrome(words):
+    for word in words:
+        if word == word[::-1]:
+            return word
+        
+    return ''
