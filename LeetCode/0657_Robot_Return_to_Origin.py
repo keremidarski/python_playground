ef7b294d4bfa33cf7d102193b5a1e7b0c839930b
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-08 22:41:59 +0200

    Add files via upload

---
+++ LeetCode/0657_Robot_Return_to_Origin.py
@@ -0,0 +1,21 @@
+# Problem description:
+# https://leetcode.com/problems/robot-return-to-origin/
+
+def judge_circle(moves):
+    start_position = (0, 0)
+    current_row = 0
+    current_column = 0
+
+    for move in moves:
+        if move == 'U':
+            current_row += 1
+        elif move == 'D':
+            current_row -= 1
+        elif move == 'R':
+            current_column += 1
+        elif move == 'L':
+            current_column -= 1
+
+    return start_position == (current_row, current_column)
+
+print(judge_circle('UD'))
