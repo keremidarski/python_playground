4d9128c29af2c0d3e3c002a70a8caaecdae1d959
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-09 11:52:22 +0200

    Update 0242_Valid_Anagram.py

--- LeetCode/0242_Valid_Anagram.py
+++ LeetCode/0242_Valid_Anagram.py
@@ -21,6 +21,6 @@ def is_anagram(s, t):
 
 
 # Expected: True
-print(is_anagram("anagram", "nagaram"))
+print(is_anagram('anagram', 'nagaram'))
 # Expected: False
-print(is_anagram("rat", "car"))
+print(is_anagram('rat', 'car'))
