06525cb316b59520596f39876e1f2e899ae87672
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-10 14:00:05 +0200

    Create 0728_Self_Dividing_Numbers.py

---
+++ LeetCode/0728_Self_Dividing_Numbers.py
@@ -0,0 +1,16 @@
+def is_self_dividing(left, right):
+    result = []
+    is_self_dividing = False
+    
+    for num in range(left, right + 1):
+        for digit in str(num):
+            if digit != '0' and num % int(digit) == 0:
+                is_self_dividing = True
+            else:
+                is_self_dividing = False
+                break
+                
+        if is_self_dividing:
+            result.append(num)
+            
+    return result
