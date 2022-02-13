7f3d26c31ff05ffe7d9bf6e81ddec007760e38de
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-13 11:16:28 +0200

    Create 2169_Count_Operations_to_Obtain_Zero.py

---
+++ LeetCode/2169_Count_Operations_to_Obtain_Zero.py
@@ -0,0 +1,12 @@
+def count_operations(num1, num2):
+    operations = 0
+
+    while num1 > 0 and num2 > 0:
+        if num1 >= num2:
+            num1 -= num2
+            operations += 1
+        else:
+            num2 -= num1
+            operations += 1
+
+    return operations
