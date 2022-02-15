e17eeef96d4cc3c52a60b2e51d27f4783b4a8a67
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-15 15:23:20 +0200

    Create 0136_Single_Number.py

---
+++ LeetCode/0136_Single_Number.py
@@ -0,0 +1,9 @@
+def single_number(nums):
+    num_occurances = {}
+    
+    for num in nums:
+        num_occurances[num] = num_occurances.get(num, 0) + 1
+    
+    for key, value in num_occurances.items():
+        if value == 1:
+            return key
