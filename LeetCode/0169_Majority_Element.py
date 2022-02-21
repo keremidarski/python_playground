7566e54beff44c1b2690a2d756a93e617968b649
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-21 15:07:33 +0200

    Create 0169_Majority_Element.py

---
+++ LeetCode/0169_Majority_Element.py
@@ -0,0 +1,14 @@
+def majority_element(nums):
+    count = {}
+    result = 0
+    max_count = 0
+
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+
+        if count[num] > max_count:
+            result = num
+
+        max_count = max(count[num], max_count)
+
+    return result
