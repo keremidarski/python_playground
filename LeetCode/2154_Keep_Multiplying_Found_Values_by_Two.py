d4f493cdb8072b652c061275619760f440e9b92c
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-02-26 17:24:27 +0200

    Update 2154_Keep_Multiplying_Found_Values_by_Two.py

--- LeetCode/2154_Keep_Multiplying_Found_Values_by_Two.py
+++ LeetCode/2154_Keep_Multiplying_Found_Values_by_Two.py
@@ -9,6 +9,6 @@ def find_final_value(nums, original):
 
 
 # Expected: 24
-print(find_final_value([5,3,6,1,12], 3))
+print(find_final_value([5, 3, 6, 1, 12], 3))
 # Expected: 4
-print(find_final_value([2,7,9], 4))
+print(find_final_value([2, 7, 9], 4))
