c9e4d991b680691d6985f09c296bf5a12ea89421
keremidarski <ivaylo.keremidarski@gmail.com>
2022-03-06 13:58:04 +0200

    New problem

---
+++ LeetCode/0094_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,13 @@
+# Problem description:
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+def inorder_traversal(root):
+    pass
+
+
+# Expected: [1, 3, 2]
+print(inorder_traversal([1, None, 2, 3]))
+# Expected: []
+print(inorder_traversal([]))
+# Expected: [1]
+print(inorder_traversal([1]))
