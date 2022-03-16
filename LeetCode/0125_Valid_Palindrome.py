6aaabf59cf03e9bcb3ef912a23a18745f24a42f8
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-03-16 17:02:13 +0200

    Update: Added a test.

--- LeetCode/0125_Valid_Palindrome.py
+++ LeetCode/0125_Valid_Palindrome.py
@@ -6,3 +6,7 @@ def is_palindrome(s):
     new_s = ''.join(char for char in s.lower() if char.isalnum())
     
     return new_s == new_s[::-1]
+
+
+# Expected: True
+print(is_palindrome('A man, a plan, a canal: Panama'))
