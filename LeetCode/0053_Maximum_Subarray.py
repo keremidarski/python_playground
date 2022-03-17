3a6e860300b2fe90e9f15d14b13c762853e2d2ec
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-03-17 14:09:52 +0200

    New problem.

---
+++ LeetCode/0053_Maximum_Subarray.py
@@ -0,0 +1,12 @@
+def max_sub_array(nums):
+    max_sub = nums[0]
+    curr_sum = 0
+
+    for n in nums:
+        if curr_sum < 0:
+            curr_sum = 0
+        
+        curr_sum += n
+        max_sub = max(max_sub, curr_sum)
+
+    return max_sub
