d5fd820c0c633d0ecf01fbe8e6f7485236e76405
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-04-01 13:36:04 +0300

    Update: Added a test.

--- LeetCode/0344_Reverse_String2.py
+++ LeetCode/0344_Reverse_String2.py
@@ -8,3 +8,9 @@ def reverse_string(s):
     while l < r:
         s[l], s[r] = s[r], s[l]
         l, r = l + 1, r - 1
+
+
+# Expected: ['o','l','l','e','h']
+s = ['h','e','l','l','o']
+reverse_string(s)
+print(s)
