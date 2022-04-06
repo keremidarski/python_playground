d45548c39f0cb538e94f4c76d00af36bb879233f
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-04-06 10:24:10 +0300

    Update: Formatting change.

--- LeetCode/0001_Two_Sum.py
+++ LeetCode/0001_Two_Sum.py
@@ -1,6 +1,7 @@
 # Problem description:
 # https://leetcode.com/problems/two-sum/
 
+
 def two_sum(nums, target):
     d = {}
     for i, n in enumerate(nums):
@@ -10,5 +11,6 @@ def two_sum(nums, target):
         else:
             d[n] = i
 
+            
 # Expected: [1, 2]
 print(two_sum([3, 2, 4], 6))
