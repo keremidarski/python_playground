30ce2f6d1c8a80951d4a181005a602b1f6198c5c
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-04-19 09:33:55 +0300

    Update: Fixed the formatting.

--- LeetCode/0771_Jewels_and_Stones.py
+++ LeetCode/0771_Jewels_and_Stones.py
@@ -1,14 +1,17 @@
 # Problem description:
 # https://leetcode.com/problems/jewels-and-stones/
 
+
 def jewels_in_stones(jewels, stones):
     counter = 0
+    
     for stone in stones:
         for jewel in jewels:
             if stone == jewel:
                 counter += 1
     
     return counter
+
 
 # Expected: 3
 print(jewels_in_stones('aA', 'aAAbbbb'))
