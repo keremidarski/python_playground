c590f5856d4d4ff9418d1e1bee84481d943f0fa0
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-06-09 12:57:19 +0300

    Update 1389_Create_Target_Array_in_the_Given_Order.py

--- LeetCode/1389_Create_Target_Array_in_the_Given_Order.py
+++ LeetCode/1389_Create_Target_Array_in_the_Given_Order.py
@@ -1,6 +1,7 @@
 # Problem description:
 # https://leetcode.com/problems/create-target-array-in-the-given-order/
 
+
 def create_target_array(nums, index):
     target = []
 
@@ -8,6 +9,7 @@ def create_target_array(nums, index):
         target.insert(index[i], nums[i])
         
     return target
+
 
 # Expected: [0, 4, 1, 3, 2]
 print(create_target_array([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]))
