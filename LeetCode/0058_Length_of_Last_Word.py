abc99beaf5c050ceaef20e4b44e02059c07edd2b
Ivaylo Keremidarski <12044844+keremidarski@users.noreply.github.com>
2023-08-12 14:46:47 +0300

    Update 0058_Length_of_Last_Word.py

--- LeetCode/0058_Length_of_Last_Word.py
+++ LeetCode/0058_Length_of_Last_Word.py
@@ -1,2 +1,12 @@
+# Problem description:
+# https://leetcode.com/problems/length-of-last-word/
+
 def get_len_of_last_word(string):
   return len(string.strip().split(" ")[-1])
+
+           
+# Expected: 6
+print(get_len_of_last_word("luffy is still joyboy")
+# Expected: 4
+print(get_len_of_last_word("   fly me   to   the moon  ")
+      
